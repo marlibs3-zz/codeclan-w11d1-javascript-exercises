d2c6a57699b7e0e5e0da21fe2700fa5e7bbbfd31
Marta Beveridge <marta@beveridge.uk>
2018-05-14 10:54:19 +0100

    Control flow file using if

---
+++ control_flow.js
@@ -0,0 +1,11 @@
+if (1 > 0) {
+  var message = "1 is greater than 0";
+}
+else if (1 == 0) {
+  var message = "1 is equal to 0";
+}
+else {
+  var message = "1 is not greater than 0"
+}
+
+console.log(message);
