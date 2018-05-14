490bf0cc43d92685016a28237a50e307e8408e3f
Marta Beveridge <marta@beveridge.uk>
2018-05-14 11:53:30 +0100

    control flow and arrays

--- control_flow.js
+++ control_flow.js
@@ -5,7 +5,7 @@ else if (1 == 0) {
   var message = "1 is equal to 0";
 }
 else {
-  var message = "1 is not greater than 0"
+  var message = "1 is not greater than 0";
 }
 
 console.log("Result:", message);
---
+++ loops_arrays_objects.js
@@ -0,0 +1,11 @@
+var sports = ['football', 'tennis', 'rubgy'];
+
+var firstSport = sports[0];
+
+console.log('first sport:', firstSport);
+console.log('second sport:', sports[1]);
+
+sports.push('snooker');
+sports.push('darts');
+
+console.log(sports);
